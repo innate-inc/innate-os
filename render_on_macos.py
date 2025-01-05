b7637c035ac3e319610d61c5ca078a481745f619
Axel Peytavin <peytavin@stanford.edu>
2025-01-05 19:55:51 +0100

    Add turtlebot and make it move

--- render_on_macos.py
+++ render_on_macos.py
@@ -1,12 +1,9 @@
 import argparse
 
-import torch
-
 import genesis as gs
 
 
 def main():
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--vis", action="store_true", default=True)
     args = parser.parse_args()
@@ -15,7 +12,6 @@ def main():
     gs.init(backend=gs.gpu)
 
     ########################## create a scene ##########################
-
     scene = gs.Scene(
         sim_options=gs.options.SimOptions(),
         viewer_options=gs.options.ViewerOptions(
@@ -32,19 +28,36 @@ def main():
 
     ########################## entities ##########################
     plane = scene.add_entity(gs.morphs.Plane())
+
+    # Load your TurtleBot3 URDF
     r0 = scene.add_entity(
-        gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"),
+        gs.morphs.URDF(file="urdf/turtlebot3_burger.urdf"),
     )
 
     ########################## build ##########################
     scene.build()
 
-    gs.tools.run_in_another_thread(fn=run_sim, args=(scene, args.vis))
+    # ------------------------------------------------------------
+    # NEW CODE: Identify the wheel joints and set gains
+    # ------------------------------------------------------------
+    left_idx = r0.get_joint("wheel_left_joint").dof_idx_local
+    right_idx = r0.get_joint("wheel_right_joint").dof_idx_local
+
+    # For velocity control, we usually set 'Kv' (velocity gain)
+    # (You can also set Kp if you prefer or if your wheels are in a PD control mode)
+    r0.set_dofs_kv([1.0, 1.0], [left_idx, right_idx])
+    # r0.set_dofs_kp([0.0, 0.0], [left_idx, right_idx])  # Typically 0 if purely velocity-based
+    # ------------------------------------------------------------
+
+    # We'll pass the robot and joint indices into run_sim so we can command them there
+    gs.tools.run_in_another_thread(
+        fn=run_sim, args=(scene, args.vis, r0, left_idx, right_idx)
+    )
     if args.vis:
         scene.viewer.start()
 
 
-def run_sim(scene, enable_vis):
+def run_sim(scene, enable_vis, robot, left_idx, right_idx):
     from time import time
 
     t_prev = time()
@@ -52,13 +65,21 @@ def run_sim(scene, enable_vis):
     while True:
         i += 1
 
+        # ------------------------------------------------------------
+        # NEW CODE: Command some constant forward velocity on each wheel
+        # ------------------------------------------------------------
+        robot.control_dofs_velocity([2.0, 2.0], [left_idx, right_idx])
+        # The wheels will rotate with velocity ~2 rad/s each step, moving the robot forward.
+        # Adjust up/down to see faster/slower movement.
+        # ------------------------------------------------------------
+
+        # Step the simulation
         scene.step()
 
         t_now = time()
-        print(1 / (t_now - t_prev), "FPS")
+        fps = 1.0 / (t_now - t_prev)
+        print(f"{fps:.2f} FPS")
         t_prev = t_now
-        # if i > 200:
-        # break
 
     if enable_vis:
         scene.viewer.stop()
