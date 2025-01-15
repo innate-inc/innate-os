a1c42a817758e4ae2fdbd312896b984eb0cdfb4b
Vignesh Anand <vignesh.anand.1503@gmail.com>
2025-01-15 12:34:39 -0800

    chnaged serial port

--- dds/setup_dds.zsh
+++ dds/setup_dds.zsh
@@ -9,8 +9,7 @@ export ROS_DOMAIN_ID=0
 export ROS_DISCOVERY_SERVER_IP=192.168.1.124
 export ROS_DISCOVERY_SERVER_PORT=11811
 export ROS_DISCOVERY_SERVER=$ROS_DISCOVERY_SERVER_IP:$ROS_DISCOVERY_SERVER_PORT
-# Create directory if it doesn't exist
-mkdir -p $HOME/maurice-prod/dds
+
 
 # Generate DDS configuration file from template
 export FASTRTPS_DEFAULT_PROFILES_FILE=$HOME/maurice-prod/dds/super_client_configuration.xml
