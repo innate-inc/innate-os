4d724f899004b08202b4aca3dbb405e1c6e77444
Vignesh Anand <vignesh.anand.1503@gmail.com>
2025-01-15 10:08:14 -0800

    automatic dds setup

---
+++ dds/setup_dds.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+export ROS_DISCOVERY_SERVER_IP=192.168.1.120
+export ROS_DISCOVERY_SERVER_PORT=11811
+export ROS_DISCOVERY_SERVER=$ROS_DISCOVERY_SERVER_IP:$ROS_DISCOVERY_SERVER_PORT
+# Create directory if it doesn't exist
+mkdir -p $HOME/maurice-prod/dds
+
+# Generate DDS configuration file from template
+export FASTRTPS_DEFAULT_PROFILES_FILE=$HOME/maurice-prod/dds/super_client_configuration.xml
+sed -e "s/DDS_SERVER_IP/$ROS_DISCOVERY_SERVER_IP/g" \
+    -e "s/DDS_SERVER_PORT/$ROS_DISCOVERY_SERVER_PORT/g" \
+    $HOME/maurice-prod/dds/super_client_template.xml > $FASTRTPS_DEFAULT_PROFILES_FILE
