1fbb3f81dd6accdeac9a168bd45e924f1d95ce66
Axel Peytavin <peytavin@stanford.edu>
2025-02-11 00:22:17 -0800

    Some debugging tools for lost messages + comments to mark bugs

--- src/agent/agent_websocket_bridge.py
+++ src/agent/agent_websocket_bridge.py
@@ -125,9 +125,11 @@ async def inbound_loop(ws, shared_queues):
                             sender=chat.get("sender", ""),
                             text=chat.get("text", ""),
                             timestamp=chat.get("timestamp", time.time()),
+                            timestamp_put_in_queue=time.time(),
                         )
+                        print(f"[ROSBridge] Chat message added to queue: {chat_msg}")
                         try:
-                            shared_queues.chat_from_bridge.put_nowait(chat_msg)
+                            shared_queues.chat_from_bridge.put(chat_msg)
                         except queue.Full:
                             print(
                                 "[ROSBridge] chat_from_bridge queue is full; dropping chat message."
