971b4b67d6a0c1777702a25ddc1007b19408e14f
Axel Peytavin <peytavin@stanford.edu>
2025-02-27 01:18:15 -0800

    Implement directive system with dynamic loading and management of primitives associated

---
+++ ros2_ws/src/brain_client/brain_client/directives/types.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Directive(ABC):
+    """
+    Base class for all directives.
+
+    A directive provides personality and behavior guidelines for the robot,
+    along with the list of primitives that should be available when this
+    directive is active.
+    """
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """
+        The name of the directive.
+        Must be defined by every subclass.
+        """
+        pass
+
+    @abstractmethod
+    def get_primitives(self) -> List[str]:
+        """
+        Returns a list of primitive names that should be available
+        when this directive is active.
+
+        Subclasses must implement this method.
+        """
+        pass
+
+    @abstractmethod
+    def get_prompt(self) -> str:
+        """
+        Returns the prompt/description for this directive.
+        This defines the robot's personality and behavior guidelines.
+
+        Subclasses must implement this method.
+        """
+        pass
