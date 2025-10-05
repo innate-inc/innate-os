79f1c31cf9fa108275ca32016b8305bc6d82ce03
Axel <peytavin@stanford.edu>
2025-10-04 17:19:44 -0700

    Forgot one file

--- ros2_ws/src/brain/brain_client/brain_client/directives/hello_world_directive.py
+++ ros2_ws/src/brain/brain_client/brain_client/directives/hello_world_directive.py
@@ -1,35 +1,27 @@
 from typing import List
 from brain_client.directives.types import Directive
-from brain_client.message_types import TaskType
 
 
-class HelloWorldDirective(Directive):
+class HelloWorld(Directive):
     """
-    Hello World directive for the robot.
-    Makes the robot turn in place until it sees a human, then greets them with a friendly wave.
+    Makes the robot turn and greet humans with a friendly wave.
     """
 
     @property
     def name(self) -> str:
-        return "hello_world_directive"
+        return "hello_world"
 
     def get_primitives(self) -> List[str]:
-        """Return the list of primitives this directive can use"""
         return [
-            TaskType.NAVIGATE_TO_POSITION.value,
-            TaskType.WAVE.value,
+            "navigate_to_position",
+            "wave"
         ]
 
     def get_prompt(self) -> str:
         """Return the prompt that defines the robot's personality and behavior"""
-        return """You are a friendly greeting robot whose sole purpose is to say hello world to the user!
-
-Your personality:
-- Gen-z like
-- Speak in lowercase all the time, even at beginning of the sentence.
-
-Navigation instructions:
-- Don't navigate, just maybe turn around if you don't see the user.
-
-Remember: Your main goal is to say hello world to the user while waving.
+        return """
+You are a robot who can say hello world to the user.
+- Speak in lowercase.
+- Don't navigate, just turn around if you don't see the user.
+- Say hello world while waving if you see the user.
 """
