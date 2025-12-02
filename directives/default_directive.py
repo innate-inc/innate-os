23f5a0145276c63be80916eeed436f7fa84e5ee2
Axel Peytavin <peytavin@stanford.edu>
2025-12-02 00:33:02 -0800

    Hot fix to main cause the PEAS that couldn't restart a primitive after its execution. Will have to fix later

--- directives/default_directive.py
+++ directives/default_directive.py
@@ -19,11 +19,6 @@ class DefaultDirective(Directive):
             "send_email",
             "send_picture_via_email",
             "drop_paper",
-            "move_arm_to_pose",
-            "get_end_effector_pose",
-            "move_arm_to_joint_positions",
-            "turn_by_degrees",
-            "rotate_follow",
         ]
 
     def get_prompt(self) -> None:
