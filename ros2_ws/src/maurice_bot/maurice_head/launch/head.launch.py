54cdd714fa9d7e5dffd1219decb93431a75ba236
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2025-12-18 22:44:40 -0800

    bruh

--- ros2_ws/src/maurice_bot/maurice_head/launch/head.launch.py
+++ ros2_ws/src/maurice_bot/maurice_head/launch/head.launch.py
@@ -2,6 +2,10 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    with open("/tmp/head_servo_node.flag3", "w") as f:
+        f.write(f"started at {time.time()}\n")
+
+
     return LaunchDescription([
         Node(
             package='maurice_head',  # Replace with your package name
