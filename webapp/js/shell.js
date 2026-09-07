df1ced75635c43f48e5bd8752247847330b8cfe6
Axel Peytavin <peytavin@stanford.edu>
2026-09-06 20:24:55 -0700

    Remove the mobile native-app promotion

--- webapp/js/shell.js
+++ webapp/js/shell.js
@@ -11,7 +11,6 @@ import { getConfig } from "./config.js";
 import { sharedAgentState } from "./teleop/agentState.js";
 import { createAgentIndicator } from "./agentIndicator.js";
 import { createArmAlert } from "./armAlert.js";
-import { maybeShowAppPromo } from "./appPromo.js";
 import { installPressActivate } from "./pressActivate.js";
 import { FOOTER_SECTIONS, GROUPS, SECTIONS, SIM_SECTIONS, railRows } from "./railLayout.js";
 
@@ -183,9 +182,6 @@ export function initShell(navigate) {
   void getConfig().then((config) => {
     if (!config?.simControls) createArmAlert(ros);
   });
-
-  // On a phone/tablet, nudge toward the native app (shown once, then remembered).
-  maybeShowAppPromo("/");
 
   /**
    * Reflect the active section: highlight its rail link, hide the agent pill on
